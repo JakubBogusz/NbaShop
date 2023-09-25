4dd4e8f97b1281e55f4486e58900acf275e37077
Jakub Bogusz <jbogusz03@gmail.com>
2023-09-25 21:53:49 +0200

    Add Product Card and List components, load images

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <div className="App">
-      <h1>Nba Shop</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
   );
 }
 
