db25e941a9698c0b7c5431888e81a87ff0923df1
Jakub Bogusz <jbogusz03@gmail.com>
2023-09-26 21:48:56 +0200

    Setup and update ProductList component on website

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,6 +1,6 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
-import { useAppSelector } from "../../app/store/configureStore";
+// import { useAppSelector } from "../../app/store/configureStore";
 import ProductCard from "./ProductCard";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 
@@ -9,16 +9,17 @@ interface Props {
 }
 
 export default function ProductList({ products }: Props) {
-    const { productsLoaded } = useAppSelector(state => state.catalog);
+    // const { productsLoaded } = useAppSelector(state => state.catalog);
     return (
         <Grid container spacing={4}>
             {products.map(product => (
                 <Grid key={product.id} item xs={4}>
-                    {!productsLoaded ? (
+                    <ProductCard product={product} />
+                    {/* {!productsLoaded ? (
                         <ProductCardSkeleton />
                     ) : (
                         <ProductCard product={product} />
-                    )}
+                    )} */}
                 </Grid>
             ))}
         </Grid>
