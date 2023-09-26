7300bc788971ad1c4f75e46f9a11ae23bbe4aa50
Jakub Bogusz <jbogusz03@gmail.com>
2023-09-26 22:10:11 +0200

    Add react-router-dom

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/materia
 import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -24,7 +25,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   );
